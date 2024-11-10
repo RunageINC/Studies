96e18e76064d87cb0ac385912347d29a975020e5
RunageINC <44951596+RunageINC@users.noreply.github.com>
2024-11-09 22:27:38 -0300

    Cleanup
    Cleanup

--- RocketSeat/ReactJS/01-IniciandoComReact/fundamentos-react-js/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import { header } from "./Header.module.css";
-
-export function Header() {
-  return (
-    <header className={header}>
-      <strong>Ignite Feed</strong>
-    </header>
-  );
-}
--- RocketSeat/ReactJS/01-IniciandoComReact/fundamentos-react-js/src/elementConstants.js
+++
@@ -1,2 +0,0 @@
-export const PARAGRAPH = "paragraph";
-export const LINK = "link";
