d06f74f59780900ff04531b12a592f77b1afeb22
RunageINC <44951596+RunageINC@users.noreply.github.com>
2025-03-26 22:00:43 -0300

    projects

---
+++ Udemy/NestJS/messages/src/messages/messages.controller.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('/messages')
+export class MessagesController {
+  constructor(public readonly messagesService: MessagesService) {}
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
+
+    return message;
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.createMessage(body.content);
+  }
+}
